28432a137f1474960af304d4c830039115b39342
Juan David <juanemanueldavidzaracho@gmail.com>
2023-08-07 16:16:49 -0400

    concatenar al presionar los botones

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.calculadora
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
@@ -10,12 +11,18 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tvRes = findViewById(R.id.bigual)
+        tvRes = findViewById(R.id.resultado)
 
 
     }
     fun calcular(view:View){
-
+        var boton = view as Button
+        var textoBoton = boton.text.toString()
+        var concatenar = tvRes?.text.toString()+textoBoton
+        tvRes?.text = concatenar
+    }
+    fun quitarCerosIzquierda(){
+        //falta por realizar
     }
 }
 
