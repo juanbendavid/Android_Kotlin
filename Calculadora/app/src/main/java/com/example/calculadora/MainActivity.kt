17e8d5d690a7901b8377c43fe51235961f24bfa2
Juan David <juanemanueldavidzaracho@gmail.com>
2023-08-06 20:56:47 -0400

    funcion calcular en proceso

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.calculadora
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+    var tvRes:TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvRes = findViewById(R.id.bigual)
+
+
+    }
+    fun calcular(view:View){
 
     }
 }
