41e418a1fe6c3c6848c556b3e15a8c5607377481
Juan David <juanemanueldavidzaracho@gmail.com>
2023-08-18 14:06:32 -0400

    imagen de DetailMejorado

--- MyMovies/app/src/main/java/com/example/mymovies/MainActivity.kt
+++ MyMovies/app/src/main/java/com/example/mymovies/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 
 import com.example.mymovies.databinding.ActivityMainBinding
@@ -13,13 +14,12 @@ import com.example.mymovies.model.MovieDbClient
 import kotlinx.coroutines.launch
 
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         
-        hideNavegationBarBottom()
         val moviesAdapter = MoviesAdapter(emptyList()) {
             navigateTo(it)
         }
@@ -45,15 +45,6 @@ class MainActivity : ComponentActivity() {
 
     }
 
-    private fun hideNavegationBarBottom() {
-        window.decorView.apply {
-            // Hide both the navigation bar and the status bar.
-            // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
-            // a general rule, you should design your app to hide the status bar whenever you
-            // hide the navigation bar.
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
-        }
-    }
 
     private fun navigateTo(it: Movie) {
         val intent = Intent(this, DetailActivity::class.java)
